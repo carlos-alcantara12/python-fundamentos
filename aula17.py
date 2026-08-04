2e6fb2e1218337057857dd79f89436f42f24ab07
carlos-alcantara12 <carlos.machado.alcantara12@gmail.com>
2026-08-04 16:14:48 -0400

    Update aula17.py

--- aula17.py
+++ aula17.py
@@ -5,9 +5,7 @@ frase = ('o python é uma linguagem de programação '
 # Utilizamos ".count()" para fazer a contagem de elementos.
 print(frase.count('a'))
 
-# CORRIGIDO: 'print(letra_atual)' e 'i += 1' estavam sem indentação,
-# ou seja, fora do corpo do while. Como 'i' nunca era incrementado
-# dentro do loop, isso causava um loop infinito.
+
 i = 0
 while i < len(frase):
     letra_atual = frase[i]
