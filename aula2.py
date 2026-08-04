09203a16f21b829077844775e1228ab93a845aa2
carlos-alcantara12 <carlos.machado.alcantara12@gmail.com>
2026-08-04 16:15:26 -0400

    Update aula2.py

--- aula2.py
+++ aula2.py
@@ -1,9 +1,7 @@
 # \n -> LF
 # \r\n -> CRLF
 
-# CORRIGIDO: o original usava barra normal ('/n', '/r/n'), que é só
-# texto literal e não tem efeito de quebra de linha. A sequência de
-# escape correta usa contrabarra: '\n' e '\r\n'.
+
 print("123, 234", sep=" ---", end='\r\n')
 print("567, 8910", sep=" ---", end='\n')
 
